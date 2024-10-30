1a03d1cc9c32f82010f336f12ed9460f47b33cc0
Nicholas <nicholas.griffin@bbc.co.uk>
2024-10-30 23:50:11 +0000

    chore: creating an initial structure and setup

---
+++ src/main.py
@@ -0,0 +1,65 @@
+import re
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def extract_main_content(text):
+    start_marker = "*** START OF THE PROJECT GUTENBERG EBOOK ALICE'S ADVENTURES IN WONDERLAND ***"
+    end_marker = "THE END"
+    
+    start_index = text.find(start_marker) + len(start_marker)
+    end_index = text.find(end_marker)
+    
+    return text[start_index:end_index]
+
+def split_into_paragraphs(text):
+    return text.split('\n\n')
+
+def clean_text(paragraphs):
+    cleaned_paragraphs = []
+    for paragraph in paragraphs:
+        # Remove unwanted patterns
+        paragraph = re.sub(r'\[.*?\]', '', paragraph)
+        paragraph = re.sub(r'\*{2,}', '', paragraph)
+        paragraph = re.sub(r'^\s*$', '', paragraph)
+        cleaned_paragraphs.append(paragraph.strip())
+        
+        """
+        TODO: Need to remove other initial content like:
+        Alice’s Adventures in Wonderland
+
+        by Lewis Carroll
+
+        THE MILLENNIUM FULCRUM EDITION 3.0
+
+        Contents
+
+        CHAPTER I.     Down the Rabbit-Hole
+        """
+    return [p for p in cleaned_paragraphs if p]
+
+def main():
+    """
+    TODO:
+    
+    - Will need to vectorize the text within the book into CloudFlare Vectorize.
+    - Use a RAG model to generate responses to questions about the book.
+    - Will need to build a simple API to interact with the chatbot.
+    - Will need to build a simple frontend to interact with the chatbot.
+    """
+    
+    file_path = './data/alice-in-wonderland.txt'
+    text = read_file(file_path)
+    main_content = extract_main_content(text)
+    paragraphs = split_into_paragraphs(main_content)
+    cleaned_paragraphs = clean_text(paragraphs)
+    
+    # For now, just print the first few cleaned paragraphs
+    for paragraph in cleaned_paragraphs[:5]:
+        print(paragraph)
+        print()
+
+
+if __name__ == "__main__":
+    main()
